993bf9b08d7a153c02652e36474129e49406c5e7
Prawee Wongsa <prawee.w@integra8t.com>
2017-10-25 14:55:55 +0700

    create default controller for our restful

---
+++ controllers/Controller.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @Author: Prawee Wongsa <prawee@integra8t.com>
+ * @Date: 10/25/2017 AD 2:49 PM
+ */
+namespace i8\controllers;
+
+class Controller extends \yii\rest\Controller
+{
+    public $layout = false;
+}
